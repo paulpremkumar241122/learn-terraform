0e42a784ade34f6dfc7cf95ac8b5b599c26786cc
Paul Prem Kumar <paul.prem5@gmail.com>
2024-01-02 01:39:35 +0530

    printing public ip

--- 06-module-1/main.tf
+++ 06-module-1/main.tf
@@ -1,17 +1,17 @@
-module "sample" {
-  source = "./ec2"
-  name = "sample"
-}
-
 module "sample1" {
   source = "./ec2"
   name = "sample1"
 }
 
+module "sample2" {
+  source = "./ec2"
+  name = "sample2"
+}
+
 output "public_ip_sample" {
-  value = module.sample.public_ip
+  value = module.sample1.public_ip
 }
 
 output "public_ip_sample1" {
-  value = module.sample1.public_ip
+  value = module.sample2.public_ip
 }
