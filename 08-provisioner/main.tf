1ec39e13d874bdad90f66402717e460762aec249
Paul Prem Kumar <paul.prem5@gmail.com>
2024-01-03 02:14:57 +0530

    try frontend

---
+++ 08-provisioner/main.tf
@@ -0,0 +1,29 @@
+resource "aws_instance" "web" {
+  ami                    = data.aws_ami.example.id
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-035198221e16083de"]
+
+  tags = {
+    Name = "HelloWorld"
+  }
+
+  provisioner "remote-exec" {
+
+    connection {
+      type     = "ssh"
+      user     = "centos"
+      password = "DevOps321"
+      host     = self.public_ip
+    }
+    inline = [
+      "sudo labauto ansible",
+      "ansible-pull -i localhost, -U https://github.com/paulpremkumar241122/roboshop-ansible.git main.yml -e role_name=frontend"
+    ]
+  }
+}
+
+data "aws_ami" "example" {
+  owners      = ["973714476881"]
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+}
