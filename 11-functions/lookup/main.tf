bba2c42c542f72c29e64602222db9dbcbd4d8552
Paul Prem Kumar <paul.prem5@gmail.com>
2024-01-04 03:22:23 +0530

    lookup

---
+++ 11-functions/lookup/main.tf
@@ -0,0 +1,19 @@
+variable "classes" {
+  default = {
+    devops = {
+      name   = "devops"
+      topics = ["jenkins", "docker"]
+    }
+    aws = {
+      name = "aws"
+    }
+  }
+}
+
+output "devops_topics" {
+  value = var.classes["devops"]["topics"]
+}
+
+output "aws_topics" {
+  value = lookup(lookup(var.classes, "aws", null), "topics", "m levu laavada")
+}
